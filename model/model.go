5e2ccdbaa56f2781d513096130be9bd1fdc53a5e
ThiagoLopes <thiagolopes@protonmail.com>
2018-08-21 22:53:23 -0300

    remove CreateXAt from SerialData struct, is not a good idea

--- model/model.go
+++ model/model.go
@@ -3,12 +3,11 @@ package model
 import "time"
 
 type SerialData struct {
-	Id            int
-	Humidity      float64
-	Temperature   float64
-	CO            float64
-	CO2           float64
-	MP25          float64
-	CreatedDateAt time.Time
-	CreatedTimeAt time.Time
+	Id          int
+	Humidity    float64
+	Temperature float64
+	CO          float64
+	CO2         float64
+	MP25        float64
+	CreatedAt   time.Time
 }
