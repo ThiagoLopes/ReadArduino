aaef6d1b234ebd6d9787114d03df41c6edf6fde5
ThiagoLopes <thiagolopes@protonmail.com>
2018-08-22 21:32:46 -0300

    update main to run new modules

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	mserial "github.com/ThiagoLopes/ReadArduino/serial"
+	"github.com/ThiagoLopes/ReadArduino/model"
 	"github.com/tarm/serial"
 	"log"
 	"os"
 	"time"
 )
 
-var BUFFER_READ [][]byte
 var TOKEN = []byte("1")
 var SERIAL_PATH string
 
 const BAUD = 9600
+const DATABASE = "serial.db"
 
 func init() {
 	if env := os.Getenv("ARDUINO"); env == "" {
@@ -29,12 +30,20 @@ func main() {
 		Baud:        BAUD,
 		ReadTimeout: time.Second / 2, // RUDE
 	}
+
+	db := model.InitDB(DATABASE) // init db
+	model.CreateTable(db)
+	defer db.Close()
+
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Panic(err)
 	}
+
 	s.Flush() // Clean data before start read
-	go mserial.LoopWriteAndRead(s, &TOKEN, &BUFFER_READ)
-	fmt.Scanln()
-	fmt.Println(BUFFER_READ)
+
+	go mserial.LoopWriteAndReadAndSave(s, &TOKEN, db) //start write, read and save
+
+	fmt.Scanln() // just dont close pls
+	fmt.Println(model.Read(db))
 }
