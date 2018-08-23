1afd71cbf95eb8607f544910e503e2b6e34a878e
ThiagoLopes <thiagolopes@protonmail.com>
2018-08-22 21:32:39 -0300

    add some tests for models

---
+++ model/model_test.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"log"
+	"testing"
+)
+
+func TestNewSerialData(t *testing.T) {
+	test_data := []byte("33.3,44.33,444.44,33.3,55.55")
+
+	out := NewSerialData(test_data)
+	log.Println(out)
+
+
+}
